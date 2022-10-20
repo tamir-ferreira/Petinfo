0a88e1b42fea5d7465b5e9998841ba6f0e7574a1
Tamir S. Ferreira <bigtam_rj@yahoo.com.br>
2022-10-20 15:26:40 -0300

    finalizado js página de cadastro

--- src/scripts/login.js
+++ src/scripts/login.js
@@ -1,21 +1,38 @@
 import { login } from "./requests.js"
+const btnLogin = document.querySelector('#btn-login')
+
 
 
 const eventLogin = () => {
     const form = document.querySelector("form")
     const elements = [...form.elements]
 
-    form.addEventListener("submit", async (event) =>{
+   /*  elements.forEach(elem => {
+        if (elem.tagName == "INPUT") {
+            console.log(elem)
+            elem.onkeyup = () => {
+                if (elem.value == '') {
+                    btnLogin.setAttribute('disabled', true)
+                    console.log('true')
+                } else {
+                    btnLogin.setAttribute('disabled', false)
+                    console.log('false')
+                }
+            }
+        }
+        // console.log(elem)
+    }) */
+
+    form.addEventListener("submit", async (event) => {
         event.preventDefault()
-  
+
         const body = {}
-        
+
         elements.forEach(elem => {
             // console.log(elem.tagName)
             if (elem.tagName == "INPUT" && elem.value != '') {
                 body[elem.id] = elem.value
             }
-
         })
         // console.log(body)
         await login(body)
--- src/scripts/register.js
+++ src/scripts/register.js
@@ -1,15 +1,39 @@
 import { register } from "./requests.js"
-
+const btnRegister = document.getElementById('btn-register')
+const btnSpinner = document.getElementById('btn-spinner')
 
 const eventRegister = () => {
     const form = document.querySelector("form")
     const elements = [...form.elements]
-
-    form.addEventListener("submit", async (event) =>{
+    btnRegister.setAttribute('disabled', true)
+    elements.forEach(e => {
+        e.onkeyup = () => {
+            const filtered = elements.filter(e => e.tagName == "INPUT")
+            const verify = filtered.every(e => e.value != '')
+            console.log(verify)
+            if (verify) {
+                // btnRegister.onclick = "none";
+                btnRegister.removeAttribute('disabled')
+                btnRegister.classList.remove('btn-gray')
+                btnRegister.classList.add('btn-brand')
+                // formActivate()
+            } else {
+                btnRegister.setAttribute('disabled', true)
+                btnRegister.classList.remove('btn-brand')
+                btnRegister.classList.add('btn-gray')
+            }
+        }
+    })
+    // btnRegister.setAttribute('disabled', false)
+    // function formActivate() {
+    form.addEventListener("submit", async (event) => {
+        console.log('submit')
         event.preventDefault()
-  
+
+        btnRegister.style.display = 'none';
+        btnSpinner.style.display = 'flex';
         const body = {}
-        
+
         elements.forEach(elem => {
             // console.log(elem.tagName)
             if (elem.tagName == "INPUT" && elem.value != '') {
@@ -19,7 +43,22 @@ const eventRegister = () => {
         })
         console.log(body)
         await register(body)
+
+        elements.forEach(elem => {
+            // console.log(elem.tagName)
+            if (elem.tagName == "INPUT" && elem.value != '') {
+                elem.value = ''
+            }
+
+        })
+        btnRegister.style.display = 'block';
+        btnSpinner.style.display = 'none';
+        btnRegister.setAttribute('disabled', true)
+        btnRegister.classList.remove('btn-brand')
+        btnRegister.classList.add('btn-gray')
     })
+    // }
 }
+
 
 eventRegister()
